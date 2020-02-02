ce2788715b31ec36d1e12389d651cebb976b1514
Yoel Molinas <yoelmolinas@w205-31-v4.eduroam.dynamic.rbg.tum.de>
2020-02-02 22:55:59 +0300

    fix

--- train.py
+++ train.py
@@ -94,10 +94,11 @@ def main(_argv):
     logging.info("Saving config : %s"%str(cfg))
     logging.info("Starting training")
     model.fit(traingen,
-                steps_per_epoch=80,
+                steps_per_epoch=1000,
                 epochs=FLAGS.num_epochs,
                 verbose=1,
                 validation_data=valgen,
+                validation_steps=100,
                 callbacks=train_callbacks,
                 workers = 8,
                 max_queue_size=3)
